8fc5c8ce0f545458f8f75e512d9f11abd00751d6
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2023-12-12 21:36:40 +0000

    completes python code for part 1 day 11

---
+++ day11/galaxypath.py
@@ -0,0 +1,57 @@
+import re
+
+file = open('input.txt', 'r')
+Lines = file.readlines()
+
+emptyRows = []
+emptyColumns = []
+
+galaxyMap = []
+
+for line in Lines:
+    row = []
+    for j in range(len(line) - 1):
+        row.append(line[j])
+    galaxyMap.append(row)
+
+emptyRowCount = 0
+for i in range(len(galaxyMap)):
+    if all(position == '.' for position in galaxyMap[i]):
+        emptyRows.append(i + emptyRowCount)
+        emptyRowCount += 1
+
+emptyColumnCount = 0
+for i in range(len(galaxyMap[0])):
+    galaxies = False
+    for j in range(len(galaxyMap)):
+        if galaxyMap[j][i] == "#":
+            galaxies = True
+            break
+    if not galaxies:
+        emptyColumns.append(i + emptyColumnCount)
+        emptyColumnCount += 1
+
+for emptyColumn in emptyColumns:
+    for j in range (len(galaxyMap)):
+        galaxyMap[j].insert(emptyColumn, '.')
+
+for emptyRow in emptyRows:
+    galaxyMap.insert(emptyRow+1, galaxyMap[emptyRow])
+
+galaxies = []
+
+for i in range(len(galaxyMap)):
+    for j in range(len(galaxyMap[0])):
+        if galaxyMap[i][j] == "#":
+            galaxies.append([i,j])
+
+sum = 0
+
+def galaxyPairSummer (galaxy1, galaxy2):
+    return abs(galaxy1[0] - galaxy2[0]) + abs(galaxy1[1] - galaxy2[1])
+
+for i in range(len(galaxies)):
+    for j in range(i, len(galaxies)):
+        sum += galaxyPairSummer(galaxies[i], galaxies[j])
+
+print(sum)
